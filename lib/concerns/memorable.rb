feb90b18e73936194702a6b564c6b398aa30df4d
Fiona Lapham <fionadlapham@gmail.com>
2018-03-12 21:59:14 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,20 @@
 module Memorable
 
-  def reset_all
-    all.clear
-  end
+    module ClassMethods
 
-  def count
-    all.count
-  end
+      def reset_all
+        self.all.clear
+      end
+
+      def count
+        self.all.count
+      end
+    end
+
+    module InstanceMethods
+      
+      def initialize
+        self.class.all << self
+      end
 
 end
